1216766122f7beb909dc605cf1e38b7384dc0f3c
kakasniff21 <mnvnarang@gmail.com>
2026-03-01 23:18:32 +0530

    Refactored UC4 to use concise array initialization

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,20 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-   
         
-        String line1 = String.join("  ", " ***", " ***", "**** ", " ****");
-        String line2 = String.join("  ", "* *", "* *", "* *", "* ");
-        String line3 = String.join("  ", "* *", "* *", "* *", "* ");
-        String line4 = String.join("  ", "* *", "* *", "**** ", " *** ");
-        String line5 = String.join("  ", "* *", "* *", "* ", "    *");
-        String line6 = String.join("  ", "* *", "* *", "* ", "    *");
-        String line7 = String.join("  ", " ***", " ***", "* ", "**** ");
+        String[] oopsBanner = {
+            String.join("  ", " ***", " ***", "**** ", " ****"),
+            String.join("  ", "* *", "* *", "* *", "* "),
+            String.join("  ", "* *", "* *", "* *", "* "),
+            String.join("  ", "* *", "* *", "**** ", " *** "),
+            String.join("  ", "* *", "* *", "* ", "    *"),
+            String.join("  ", "* *", "* *", "* ", "    *"),
+            String.join("  ", " ***", " ***", "* ", "**** ")
+        };
 
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
-        System.out.println(line6);
-        System.out.println(line7);
+        // Traverse the array using an enhanced for-loop
+        for (String line : oopsBanner) {
+            System.out.println(line);
+        }
     }
 }
